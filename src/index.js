d2e4c8175e5a7a31fe0299922c7b8ec4b53d3c9a
Ben Drucker <bvdrucker@gmail.com>
2015-04-10 12:55:57 -0400

    Initial implementation

---
+++ src/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import clone from 'clone';
+import extend from 'xtend/mutable';
+
+const methods = [
+  'card.createToken',
+  'bankAccount.createToken',
+  'bitcoinReceiver.createReceiver',
+  'bitcoinReceiver.pollReceiver'
+];
+
+export default function (Stripe, Promise) {
+  if (!Promise) throw new Error('Promise constructor must be provided');
+  return methods.reduce((Stripe, method) => {
+    const [context, name] = method.split('.');
+    const fn = Stripe[context][name];
+    Stripe[context][name] = promisify(Promise, fn, context, stripeResponseHandler);
+    return Stripe;
+  }, clone(Stripe));
+}
+
+function promisify (Promise, fn, context, resolver) {
+  return function promisified (...args) {
+    return new Promise((resolve, reject) => {
+      fn.apply(context, args.concat(() => {
+        resolver.apply({resolve, reject}, arguments);
+      }));      
+    });
+  };
+}
+
+function stripeResponseHandler (status, response) {
+  if (response.error) {
+    this.reject(extend(new Error(), response.error));
+  }
+  else {
+    this.resolve(response);
+  }
+}
